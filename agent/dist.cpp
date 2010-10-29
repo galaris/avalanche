ea5dacc4ab2e056a595ca229cd3a5844f2f53e26
ild <ild@inbox.ru>
2010-10-29 18:45:33 +0000

    group get and group all

--- agent/dist.cpp
+++ agent/dist.cpp
@@ -123,210 +123,220 @@ int main(int argc, char** argv)
     if (gameBegan)
     {
       printf("iterating through starvated\n");
-      for (set<int>::iterator fd = starvating_a.begin(); fd != starvating_a.end();)
+      int size = starvating_a.size();
+      if (size > 0)
       {
         write(mainfd, "a", 1);
-        printf("sent all from %d to %d\n", *fd, mainfd);
-        int namelength, length, startdepth, invertdepth, alarm, tracegrindAlarm, threads, argsnum;
-        bool useMemcheck, leaks, traceChildren, checkDanger, debug, verbose, suppressSubcalls;
-        char buf[128];
-        READ_MAIN(buf, 1);
-//first read 1 byte - either "r" or "q"
-        if (*buf == 'q')
+        write(mainfd, &size, sizeof(int));
+        char c;
+        READ_MAIN(&c, 1);
+        //first read 1 byte - either "r" or "q"
+        if (c == 'q')
         {
           printf("main avalanche finished work\n");
           send_exit();
         }
-        filenum = 0;
-        READ_MAIN( &filenum, sizeof(int));
-        printf("filenum=%d\n", filenum);
-        if (filenum > 0)
+        for (set<int>::iterator fd = starvating_a.begin(); fd != starvating_a.end();)
         {
-          WRITE(*fd, &filenum, sizeof(int));
-          READ_MAIN( &sockets, sizeof(bool));
-          WRITE(*fd, &sockets, sizeof(bool));
-          READ_MAIN( &datagrams, sizeof(bool));
-          WRITE(*fd, &datagrams, sizeof(bool));
-          for (int j = 0; j < filenum; j ++)
+          int namelength, length, startdepth, invertdepth, alarm, tracegrindAlarm, threads, argsnum;
+          bool useMemcheck, leaks, traceChildren, checkDanger, debug, verbose, suppressSubcalls;
+          char buf[128];
+          filenum = 0;
+          READ_MAIN( &filenum, sizeof(int));
+          printf("filenum=%d\n", filenum);
+          if (filenum > 0)
           {
-            if (!sockets && !datagrams)
+            WRITE(*fd, &filenum, sizeof(int));
+            READ_MAIN( &sockets, sizeof(bool));
+            WRITE(*fd, &sockets, sizeof(bool));
+            READ_MAIN( &datagrams, sizeof(bool));
+            WRITE(*fd, &datagrams, sizeof(bool));
+            for (int j = 0; j < filenum; j ++)
             {
-              READ_MAIN( &namelength, sizeof(int));
-              printf("namelength=%d\n", namelength);
-              WRITE(*fd, &namelength, sizeof(int));
-              read(mainfd, buf, namelength);
-              buf[namelength] = '\0';
-              printf("buf=%s\n", buf);
-              write(*fd, buf, namelength);
-            }
-            READ_MAIN( &length, sizeof(int));
-            printf("length=%d\n", length);
-            WRITE(*fd, &length, sizeof(int));
-            char* file = new char[length];
-            int received = 0;
-            while (received < length)
-            {
-              int res = read(mainfd, file + received, length - received);
-              if (res == -1)
+              if (!sockets && !datagrams)
               {
-                printf("connection with main avalanche is down\n");
-                send_exit();
+                READ_MAIN( &namelength, sizeof(int));
+                printf("namelength=%d\n", namelength);
+                WRITE(*fd, &namelength, sizeof(int));
+                read(mainfd, buf, namelength);
+                buf[namelength] = '\0';
+                printf("buf=%s\n", buf);
+                write(*fd, buf, namelength);
               }
-              received += res;
+              READ_MAIN( &length, sizeof(int));
+              printf("length=%d\n", length);
+              WRITE(*fd, &length, sizeof(int));
+              char* file = new char[length];
+              int received = 0;
+              while (received < length)
+              {
+                int res = read(mainfd, file + received, length - received);
+                if (res == -1)
+                {
+                  printf("connection with main avalanche is down\n");
+                  send_exit();
+                }
+                received += res;
+              }
+              /*for (int j = 0; j < length; j++)
+              {
+                printf("%x", file[j]);
+              }*/
+              write(*fd, file, length);
+              delete []file;
             }
-            for (int j = 0; j < length; j++)
+            READ_MAIN( &startdepth, sizeof(int));
+            WRITE(*fd, &startdepth, sizeof(int));
+            READ_MAIN( &invertdepth, sizeof(int));
+            WRITE(*fd, &invertdepth, sizeof(int));
+            READ_MAIN( &alarm, sizeof(int));
+            WRITE(*fd, &alarm, sizeof(int));
+            READ_MAIN( &tracegrindAlarm, sizeof(int));
+            printf("tracegrindAlarm=%d\n", tracegrindAlarm);
+            WRITE(*fd, &tracegrindAlarm, sizeof(int));
+            READ_MAIN( &threads, sizeof(int));
+            WRITE(*fd, &threads, sizeof(int));
+            READ_MAIN( &argsnum, sizeof(int));
+            printf("argsnum=%d\n", argsnum);
+            WRITE(*fd, &argsnum, sizeof(int));
+
+            READ_MAIN( &useMemcheck, sizeof(bool));
+            WRITE(*fd, &useMemcheck, sizeof(bool));
+            READ_MAIN( &leaks, sizeof(bool));
+            WRITE(*fd, &leaks, sizeof(bool));
+            READ_MAIN( &traceChildren, sizeof(bool));
+            WRITE(*fd, &traceChildren, sizeof(bool));
+            READ_MAIN( &checkDanger, sizeof(bool));
+            WRITE(*fd, &checkDanger, sizeof(bool));
+            READ_MAIN( &debug, sizeof(bool));
+            WRITE(*fd, &debug, sizeof(bool));
+            READ_MAIN( &verbose, sizeof(bool));
+            WRITE(*fd, &verbose, sizeof(bool));
+            READ_MAIN( &suppressSubcalls, sizeof(bool));
+            WRITE(*fd, &suppressSubcalls, sizeof(bool));
+
+            if (sockets)
             {
-              printf("%x", file[j]);
+              int length;
+              READ_MAIN( &length, sizeof(int));
+              WRITE(*fd, &length, sizeof(int));
+              read(mainfd, buf, length);
+              write(*fd, buf, length);
+              int port;
+              READ_MAIN( &port, sizeof(int));
+              WRITE(*fd, &port, sizeof(int));
             }
-            write(*fd, file, length);
-            delete []file;
+
+            int masklength;
+            READ_MAIN( &masklength, sizeof(int));
+            WRITE(*fd, &masklength, sizeof(int));
+            if (masklength != 0)
+            {
+              char* mask = new char[masklength];
+              read(mainfd, mask, masklength);
+              write(*fd, mask, masklength);
+              delete[] mask;
+            }
+
+            int filtersNum;
+            READ_MAIN( &filtersNum, sizeof(int));
+            WRITE(*fd, &filtersNum, sizeof(int));
+            for (int i = 0; i < filtersNum; i++)
+            {
+              int length; 
+              READ_MAIN( &length, sizeof(int));
+              WRITE(*fd, &length, sizeof(int));
+              read(mainfd, buf, length);
+              write(*fd, buf, length);
+            }
+
+            int filterlength;
+            READ_MAIN( &filterlength, sizeof(int));
+            WRITE(*fd, &filterlength, sizeof(int));
+            if (filterlength != 0)
+            {
+              char* filter = new char[filterlength];
+              read(mainfd, filter, filterlength);
+              write(*fd, filter, filterlength);
+              delete[] filter;
+            }
+
+            for (int i = 0; i < argsnum; i++)
+            {
+              int arglength;
+              READ_MAIN( &arglength, sizeof(int));
+              printf("arglength=%d\n", arglength);
+              WRITE(*fd, &arglength, sizeof(int));
+              read(mainfd, buf, arglength);
+              buf[arglength] = '\0';
+              printf("buf=%s\n", buf);
+              write(*fd, buf, arglength);
+            }
+            set<int>::iterator to_erase = fd;
+            fd++;
+            starvating_a.erase(to_erase);
           }
-          READ_MAIN( &startdepth, sizeof(int));
-          WRITE(*fd, &startdepth, sizeof(int));
-          READ_MAIN( &invertdepth, sizeof(int));
-          WRITE(*fd, &invertdepth, sizeof(int));
-          READ_MAIN( &alarm, sizeof(int));
-          WRITE(*fd, &alarm, sizeof(int));
-          READ_MAIN( &tracegrindAlarm, sizeof(int));
-          printf("tracegrindAlarm=%d\n", tracegrindAlarm);
-          WRITE(*fd, &tracegrindAlarm, sizeof(int));
-          READ_MAIN( &threads, sizeof(int));
-          WRITE(*fd, &threads, sizeof(int));
-          READ_MAIN( &argsnum, sizeof(int));
-          printf("argsnum=%d\n", argsnum);
-          WRITE(*fd, &argsnum, sizeof(int));
-
-          READ_MAIN( &useMemcheck, sizeof(bool));
-          WRITE(*fd, &useMemcheck, sizeof(bool));
-          READ_MAIN( &leaks, sizeof(bool));
-          WRITE(*fd, &leaks, sizeof(bool));
-          READ_MAIN( &traceChildren, sizeof(bool));
-          WRITE(*fd, &traceChildren, sizeof(bool));
-          READ_MAIN( &checkDanger, sizeof(bool));
-          WRITE(*fd, &checkDanger, sizeof(bool));
-          READ_MAIN( &debug, sizeof(bool));
-          WRITE(*fd, &debug, sizeof(bool));
-          READ_MAIN( &verbose, sizeof(bool));
-          WRITE(*fd, &verbose, sizeof(bool));
-          READ_MAIN( &suppressSubcalls, sizeof(bool));
-          WRITE(*fd, &suppressSubcalls, sizeof(bool));
-
-          if (sockets)
+          else
           {
-            int length;
-            READ_MAIN( &length, sizeof(int));
-            WRITE(*fd, &length, sizeof(int));
-            read(mainfd, buf, length);
-            write(*fd, buf, length);
-            int port;
-            READ_MAIN( &port, sizeof(int));
-            WRITE(*fd, &port, sizeof(int));
+            fd++;
           }
-
-          int masklength;
-          READ_MAIN( &masklength, sizeof(int));
-          WRITE(*fd, &masklength, sizeof(int));
-          if (masklength != 0)
-          {
-            char* mask = new char[masklength];
-            read(mainfd, mask, masklength);
-            write(*fd, mask, masklength);
-            delete[] mask;
-          }
-
-          int filtersNum;
-          READ_MAIN( &filtersNum, sizeof(int));
-          WRITE(*fd, &filtersNum, sizeof(int));
-          for (int i = 0; i < filtersNum; i++)
-          {
-            int length; 
-            READ_MAIN( &length, sizeof(int));
-            WRITE(*fd, &length, sizeof(int));
-            read(mainfd, buf, length);
-            write(*fd, buf, length);
-          }
-
-          int filterlength;
-          READ_MAIN( &filterlength, sizeof(int));
-          WRITE(*fd, &filterlength, sizeof(int));
-          if (filterlength != 0)
-          {
-            char* filter = new char[filterlength];
-            read(mainfd, filter, filterlength);
-            write(*fd, filter, filterlength);
-            delete[] filter;
-          }
-
-          for (int i = 0; i < argsnum; i++)
-          {
-            int arglength;
-            READ_MAIN( &arglength, sizeof(int));
-            printf("arglength=%d\n", arglength);
-            WRITE(*fd, &arglength, sizeof(int));
-            read(mainfd, buf, arglength);
-            buf[arglength] = '\0';
-            printf("buf=%s\n", buf);
-            write(*fd, buf, arglength);
-          }
-          set<int>::iterator to_erase = fd;
-          fd++;
-          starvating_a.erase(to_erase);
-        }
-        else
-        {
-          fd++;
         }
       }
 
-      for (set<int>::iterator fd = starvating_g.begin(); fd != starvating_g.end();)
+      size = starvating_g.size();
+      if (size > 0)
       {
-        write(mainfd, "g", 1);
-        printf("sent get from %d to %d\n", *fd, mainfd);
-        bool g_successful = false;
         int length, startdepth;
-        char buf[2];
-        READ_MAIN(buf, 1);
-//first read 1 byte - either "r" or "q"
-        if (*buf == 'q')
+        write(mainfd, "g", 1);
+        write(mainfd, &size, sizeof(int));
+        char c;
+        READ_MAIN(&c, 1);
+        //first read 1 byte - either "r" or "q"
+        if (c == 'q')
         {
           printf("main avalanche finished work\n");
           send_exit();
         }
-        for (int j = 0; j < filenum; j ++)
+        for (set<int>::iterator fd = starvating_g.begin(); fd != starvating_g.end();)
         {
-          READ_MAIN(&length, sizeof(int));
+          for (int j = 0; j < filenum; j ++)
+          {
+            READ_MAIN(&length, sizeof(int));
+            if (length > 0)
+            {
+              WRITE(*fd, &length, sizeof(int));
+              char* file = new char[length];
+              int received = 0;
+              while (received < length)
+              {
+                int res = read(mainfd, file + received, length - received);
+                if (res == -1)
+                {
+                  printf("connection with main avalanche is down\n");
+                  send_exit();
+                }
+                received += res;
+              }
+              write(*fd, file, length);
+              delete []file;
+            }
+            else 
+            {
+              break;
+            }
+          }
           if (length > 0)
           {
-            WRITE(*fd, &length, sizeof(int));
-            char* file = new char[length];
-            int received = 0;
-            while (received < length)
-            {
-              int res = read(mainfd, file + received, length - received);
-              if (res == -1)
-              {
-                printf("connection with main avalanche is down\n");
-                send_exit();
-              }
-              received += res;
-            }
-            g_successful = true;
-            write(*fd, file, length);
-            delete []file;
+            READ_MAIN(&startdepth, sizeof(int));
+            WRITE(*fd, &startdepth, sizeof(int));
+            set<int>::iterator to_erase = fd;
+            fd++;
+            starvating_g.erase(to_erase);
           }
-          else break;      
-        }
-        if (g_successful)
-        {
-          READ_MAIN(&startdepth, sizeof(int));
-          WRITE(*fd, &startdepth, sizeof(int));
-          set<int>::iterator to_erase = fd;
-          fd++;
-          starvating_g.erase(to_erase);
-        }
-        else
-        {
-          fd++;
+          else
+          {
+            fd++;
+          }
         }
       }
     }
